2064b3818e7984f7842ebcc51661e682ca361921
Vipin <vipinrao03@gmail.com>
2021-11-01 00:06:25 +0530

    upload

---
+++ js/home.js
@@ -0,0 +1,6 @@
+const hideTop = () => {
+    document.getElementById("top").style.display = "none";
+    document.getElementById("main-header").style.borderTop= "0px solid #625750";
+    document.getElementById("side-inside-mainHeader").style.paddingTop= "0.5rem";
+
+}
